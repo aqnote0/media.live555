a1ad1c4cb4b39b2d1c648b30f80218fbad38b05f
Peng Li <aqnote@qq.com>
2018-12-11 16:36:33 +0800

    xxx
    xxx

--- proxy.sh
+++ proxy.sh
@@ -1,5 +1,5 @@
 ##!/bin/bash
 
-./proxyServer/live555ProxyServer -V -t rtsp://admin:admin12345@30.87.4.137:554/h264/ch1/main/av_stream
+./proxyServer/live555ProxyServer -V -t rtsp://xxx:xxxx@30.87.4.137:554/h264/ch1/main/av_stream
 
 exit 0
